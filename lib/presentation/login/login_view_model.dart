e9f05eba4497d9998c08ad32ef3193bdf790c2b2
kimkangtae <tt997768@gmail.com>
2024-01-23 21:51:29 +0900

    Feat: _formkey => formkey

--- lib/presentation/login/login_view_model.dart
+++ lib/presentation/login/login_view_model.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 
-class MainViewModel extends ChangeNotifier{
-  final _formKey = GlobalKey<FormState>();
+class LoginViewModel extends ChangeNotifier{
+  final formKey = GlobalKey<FormState>();
 
   // TODO 후에 model 생성 예정
   String userEmail = '';
   String userPassword = '';
 
   void tryValidation() {
-    final isValid = _formKey.currentState!.validate();
+    print(formKey.currentState);
+    final isValid = formKey.currentState!.validate();
     if(isValid) {
-      _formKey.currentState!.save();
+      formKey.currentState!.save();
     }
   }
 }
