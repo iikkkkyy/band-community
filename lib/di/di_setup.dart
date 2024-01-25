53fc66da3ed8d95addae5fe467b4f0e25a84ff93
kimkangtae <tt997768@gmail.com>
2024-01-25 18:01:00 +0900

    Feat: getIt 적용

---
+++ lib/di/di_setup.dart
@@ -0,0 +1,11 @@
+import 'package:get_it/get_it.dart';
+
+import '../presentation/login/login_view_model.dart';
+import '../presentation/signup/signup_screen_view_model.dart';
+
+final getIt = GetIt.instance;
+
+void diSetup() {
+  getIt.registerFactory(() => SignUpViewModel());
+  getIt.registerFactory(() => LoginViewModel());
+}
