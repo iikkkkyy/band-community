97b7f8c6abfe8c74b4b69b3e4db38050b31d6c22
kimkangtae <tt997768@gmail.com>
2024-01-26 11:06:35 +0900

    Feat: 프로필 설정 페이지 추가

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,5 +1,7 @@
 import 'package:band_community/presentation/login/login_screen.dart';
 import 'package:band_community/presentation/login/login_view_model.dart';
+import 'package:band_community/presentation/profile/profile_screen.dart';
+import 'package:band_community/presentation/profile/profile_view_model.dart';
 import 'package:band_community/presentation/resetpassword/reset_password_screen.dart';
 import 'package:band_community/presentation/resetpassword/reset_password_view_model.dart';
 import 'package:band_community/presentation/signup/signup_screen.dart';
@@ -8,32 +10,36 @@ import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'di/di_setup.dart';
 
-
 final router = GoRouter(
   routes: [
     GoRoute(
-      path: '/',
+        path: '/',
+        builder: (context, state) => ChangeNotifierProvider(
+              create: (_) => getIt<LoginViewModel>(),
+              child: const LoginScreen(),
+            ),
+        routes: [
+          GoRoute(
+            path: 'signup',
+            builder: (context, state) => ChangeNotifierProvider(
+              create: (_) => getIt<SignUpViewModel>(),
+              child: const SignUpScreen(),
+            ),
+          ),
+          GoRoute(
+            path: 'reset',
+            builder: (context, state) => ChangeNotifierProvider(
+              create: (_) => getIt<ResetPasswordViewModel>(),
+              child: const ResetPasswordScreen(),
+            ),
+          ),
+        ]),
+    GoRoute(
+      path: '/profile',
       builder: (context, state) => ChangeNotifierProvider(
-        create: (_) => getIt<LoginViewModel>(),
-        child: const LoginScreen(),
+        create: (_) => getIt<SignUpProfileViewModel>(),
+        child: const SignUpProfileScreen(),
       ),
-      routes: [
-        GoRoute(
-          path: 'signup',
-          builder: (context, state) => ChangeNotifierProvider(
-            create: (_) => getIt<SignUpViewModel>(),
-            child: const SignUpScreen(),
-          ),
-        ),
-        GoRoute(
-          path: 'reset',
-          builder: (context, state) => ChangeNotifierProvider(
-            create: (_) => getIt<ResetPasswordViewModel>(),
-            child: const ResetPasswordScreen(),
-          ),
-        ),
-      ]
     ),
-
   ],
 );
