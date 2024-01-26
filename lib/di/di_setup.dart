72e10a7c4f7b555d9e3299aa77e71e18c6129b86
kimkangtae <tt997768@gmail.com>
2024-01-26 10:26:13 +0900

    Feat: 비밀번호 재설정 페이지 추가

--- lib/di/di_setup.dart
+++ lib/di/di_setup.dart
@@ -1,6 +1,7 @@
 import 'package:get_it/get_it.dart';
 
 import '../presentation/login/login_view_model.dart';
+import '../presentation/resetpassword/reset_password_view_model.dart';
 import '../presentation/signup/signup_screen_view_model.dart';
 
 final getIt = GetIt.instance;
@@ -8,4 +9,5 @@ final getIt = GetIt.instance;
 void diSetup() {
   getIt.registerFactory(() => SignUpViewModel());
   getIt.registerFactory(() => LoginViewModel());
+  getIt.registerFactory(() => ResetPasswordViewModel());
 }
