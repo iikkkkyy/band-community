bfaf7bf1fa369f64109adb8d90c003626ef9c0e8
kimkangtae <tt997768@gmail.com>
2024-01-31 20:48:52 +0900

    Feat: ImagePicker 추가(upload)

--- lib/presentation/profile/profile_view_model.dart
+++ lib/presentation/profile/profile_view_model.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:image_picker/image_picker.dart';
 
-class SignUpProfileViewModel extends ChangeNotifier{
+class SignUpProfileViewModel extends ChangeNotifier {
+  late XFile _image;
+
+  XFile get image => _image; //ImagePicker...
+
+  void uploadImageToStorage(ImageSource source) async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? image = await picker.pickImage(source: ImageSource.gallery);
+
+    if (image == null) return;
+    _image = image;
+    ChangeNotifier();
+  }
+
 
 }
