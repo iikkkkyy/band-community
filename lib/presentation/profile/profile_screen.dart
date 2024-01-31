28de9171ede577d0e9075ab82d66a72e4ed3392a
kimkangtae <tt997768@gmail.com>
2024-01-31 20:48:38 +0900

    Feat: 프로필설정 이미지 / 바텀 sheet 추가

--- lib/presentation/profile/profile_screen.dart
+++ lib/presentation/profile/profile_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
+import 'package:band_community/presentation/profile/profile_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
 
 class SignUpProfileScreen extends StatefulWidget {
   const SignUpProfileScreen({super.key});
@@ -8,6 +13,14 @@ class SignUpProfileScreen extends StatefulWidget {
 }
 
 class _SignUpProfileScreenState extends State<SignUpProfileScreen> {
+  late SignUpProfileViewModel _viewModel;
+
+  @override
+  void initState() {
+    super.initState();
+    _viewModel = context.read<SignUpProfileViewModel>();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,6 +38,84 @@ class _SignUpProfileScreenState extends State<SignUpProfileScreen> {
           ),
         ),
       ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            const SizedBox(
+              height: 17,
+            ),
+            GestureDetector(
+              onTap: () {
+                _showBottomSheet();
+              },
+              child: CircleAvatar(
+                radius: 45,
+                backgroundImage: FileImage(_viewModel.image as File),
+                  onBackgroundImageError : AssetImage('assets/profile/Default.png'),
+                // AssetImage('assets/profile/Default.png'),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  _showBottomSheet() {
+    return showModalBottomSheet(
+        context: context,
+        // shape: const RoundedRectangleBorder(
+        //   borderRadius: BorderRadius.vertical(
+        //     top: Radius.circular(25),
+        //   ),
+        // ),
+        builder: (context) {
+          return Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const SizedBox(
+                height: 15,
+              ),
+              const Text(
+                '프로필 사진 설정',
+                style: TextStyle(color: Colors.black54, fontSize: 14),
+              ),
+              const Divider(
+                thickness: 0.2,
+                color: Colors.black54,
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              TextButton(
+                  onPressed: () {
+                    _viewModel.uploadImageToStorage(ImageSource.gallery);
+                  },
+                  child: const Text(
+                    '갤러리에서 사진 선택',
+                    style: TextStyle(color: Colors.blue, fontSize: 20),
+                  )),
+              const SizedBox(
+                height: 5,
+              ),
+              const Divider(
+                thickness: 0.2,
+                color: Colors.black54,
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    '사진 찍기',
+                    style: TextStyle(color: Colors.blue, fontSize: 20),
+                  )),
+              const SizedBox(
+                height: 15,
+              ),
+            ],
+          );
+        });
   }
 }
