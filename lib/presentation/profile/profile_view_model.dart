6fd3786b6307bb292d38f7a651d58286d298ce96
kimkangtae <tt997768@gmail.com>
2024-02-01 18:30:31 +0900

    Feat: Storage 업로드 기능 추가

--- lib/presentation/profile/profile_view_model.dart
+++ lib/presentation/profile/profile_view_model.dart
@@ -1,20 +1,44 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:image_picker/image_picker.dart';
 
 class SignUpProfileViewModel extends ChangeNotifier {
+  final FirebaseStorage _firebaseStorage = FirebaseStorage.instance;
+
   late XFile _image;
+  String? _userImageUrl;
+
+  ImageProvider<Object>? getUserImage() {
+    if (_userImageUrl != null) {
+      return const NetworkImage('url');
+    } else {
+      return const AssetImage('assets/profile/Default.png');
+    }
+  }
 
   XFile get image => _image; //ImagePicker...
 
   void uploadImageToStorage(ImageSource source) async {
     final ImagePicker picker = ImagePicker();
-    final XFile? image = await picker.pickImage(source: ImageSource.gallery);
+    final XFile? image = await picker.pickImage(source: source);
 
     if (image == null) return;
     _image = image;
     ChangeNotifier();
+
+    // 프로필 사진을 업로드할 경로와 파일명을 정의. 사용자의 uid를 이용하여 파일명의 중복 가능성 제거
+    // StorageReference storageReference =
+    //     _firebaseStorage.ref().child("profile/${_user.uid}");
+    TaskSnapshot task = await _firebaseStorage
+    .ref()
+    .child('profile/1')
+    .putFile(File(image.path));
+
+    final downloadUrl = await task.ref.getDownloadURL();
+    print(downloadUrl);
+
   }
-
-
 }
