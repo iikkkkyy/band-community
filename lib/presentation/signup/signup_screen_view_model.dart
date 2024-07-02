b7d41477dba2afa3da4191d6c7af4969ef8c2c3c
kimkangtae <tt997768@gmail.com>
2024-07-02 18:19:33 +0900

    feat: 회원가입 페이지 회원명 / 번호 추가

--- lib/presentation/signup/signup_screen_view_model.dart
+++ lib/presentation/signup/signup_screen_view_model.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SignUpViewModel extends ChangeNotifier{
   final formKey = GlobalKey<FormState>();
-  final authentication = FirebaseAuth.instance;
+  // final authentication = FirebaseAuth.instance;
+  final authentication = Supabase.instance.client;
+
 
   // TODO 후에 model 생성 예정
+  String userName = '';
   String userEmail = '';
   String userPassword = '';
+  String phoneNumber = '';
 
   bool tryValidation() {
     final isValid = formKey.currentState!.validate();
