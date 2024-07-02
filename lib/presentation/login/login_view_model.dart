05b5b6928e3140813d6e4767c2bab47806e80c99
kimkangtae <tt997768@gmail.com>
2024-07-02 18:18:19 +0900

    feat: firebase instance 제거

--- lib/presentation/login/login_view_model.dart
+++ lib/presentation/login/login_view_model.dart
@@ -3,7 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginViewModel extends ChangeNotifier{
   final formKey = GlobalKey<FormState>();
-  final _authentication = FirebaseAuth.instance;
+  // final _authentication = FirebaseAuth.instance;
 
   // TODO 후에 model 생성 예정
   String userEmail = '';
