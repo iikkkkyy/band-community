0101ed62561a25213416e8f0b51bb6c920114497
kimkangtae <tt997768@gmail.com>
2024-07-05 19:17:43 +0900

    feat: 닉네임 중복확인

--- lib/presentation/signup/signup_screen_view_model.dart
+++ lib/presentation/signup/signup_screen_view_model.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-class SignUpViewModel extends ChangeNotifier{
+class SignUpViewModel extends ChangeNotifier {
   final formKey = GlobalKey<FormState>();
+
   // final authentication = FirebaseAuth.instance;
   final authentication = Supabase.instance.client;
-
 
   // TODO 후에 model 생성 예정
   String userName = '';
@@ -17,12 +17,25 @@ class SignUpViewModel extends ChangeNotifier{
   bool tryValidation() {
     final isValid = formKey.currentState!.validate();
     print(isValid);
-    if(isValid) {
+    if (isValid) {
       formKey.currentState!.save();
       return true;
     } else {
       return false;
     }
+  }
 
+  Future<bool> checkUserNameExists(String userName) async {
+    final query = await authentication
+        .from('user_profile')
+        .select('id')
+        .eq('user_name', userName)
+        .limit(1);
+    print(query);
+    if (query.isEmpty || (query == "[]")) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
