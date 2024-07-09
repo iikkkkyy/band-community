262359ac41cba6f8e44b74f4a596c8e3c1fa83c6
kimkangtae <tt997768@gmail.com>
2024-07-10 00:41:59 +0900

    feat: 카카오 연동

--- lib/presentation/login/login_view_model.dart
+++ lib/presentation/login/login_view_model.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class LoginViewModel extends ChangeNotifier{
   final formKey = GlobalKey<FormState>();
   // final _authentication = FirebaseAuth.instance;
+  final authentication = Supabase.instance.client;
 
   // TODO 후에 model 생성 예정
   String userEmail = '';
