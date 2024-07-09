701982b9d6500f95d8945f62a072c94e51d7bc22
kimkangtae <tt997768@gmail.com>
2024-07-10 01:24:37 +0900

    feat: supabase storage 연동

--- lib/presentation/profile/profile_view_model.dart
+++ lib/presentation/profile/profile_view_model.dart
@@ -1,13 +1,12 @@
 import 'dart:io' as io;
-
-import 'package:firebase_storage/firebase_storage.dart';
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SignUpProfileViewModel extends ChangeNotifier {
-  // final FirebaseStorage _firebaseStorage = FirebaseStorage.instance;
+  final authentication = Supabase.instance.client;
   final formKey = GlobalKey<FormState>();
   bool vocal = true;
   bool guitar = true;
@@ -17,24 +16,11 @@ class SignUpProfileViewModel extends ChangeNotifier {
   bool manager = true;
   bool etc = true;
 
-
   XFile? image = XFile('assets/profile/Default.png');
-  String? _userImageUrl;
   bool imageFlag = false;
 
-
-  // ImageProvider<Object>? getUserImage() {
-  //   final _user = FirebaseAuth.instance.currentUser;
-  //   // return NetworkImage('https://firebasestorage.googleapis.com/v0/b/band-community.appspot.com/o/profile/${_user?.uid}');
-  //   if (_userImageUrl != null) {
-  //     return NetworkImage(_userImageUrl!);
-  //   } else {
-  //     return const AssetImage('assets/profile/Default.png');
-  //   }
-  // }
-
   ImageProvider<Object>? getUserImage() {
-    if(image == null) return const AssetImage('assets/profile/Default.png');
+    if (image == null) return const AssetImage('assets/profile/Default.png');
     if (imageFlag) {
       ChangeNotifier();
       return FileImage(io.File(image!.path));
@@ -43,39 +29,21 @@ class SignUpProfileViewModel extends ChangeNotifier {
     }
   }
 
-
   Future<void> selectImage(ImageSource source) async {
     final ImagePicker picker = ImagePicker();
     image = await picker.pickImage(source: source);
-    if(image == null) return null;
+    if (image == null) return;
     String? croppedFilePath = await _cropImage(image!);
     image = XFile(croppedFilePath!);
     imageFlag = true;
     ChangeNotifier();
-    // return _cropImage(image!);
-    // final _user =  FirebaseAuth.instance.currentUser;
-    // print(FirebaseAuth.instance.currentUser);
-    // if (image == null) return;
-    // _image = image;
-    //
-    // // 프로필 사진을 업로드할 경로와 파일명을 정의. 사용자의 uid를 이용하여 파일명의 중복 가능성 제거
-    // // StorageReference storageReference =
-    // //     _firebaseStorage.ref().child("profile/${_user.uid}");
-    // TaskSnapshot task = await _firebaseStorage
-    // .ref()
-    // .child('profile/${_user!.uid}')
-    // .putFile(File(image.path));
-    //
-    // final downloadUrl = await task.ref.getDownloadURL();
-    // _userImageUrl = downloadUrl;
-    // ChangeNotifier();
-    // print(downloadUrl);
   }
+
   Future<String?> _cropImage(XFile file) async {
     final croppedFile = await ImageCropper().cropImage(
       sourcePath: file.path,
       compressFormat: ImageCompressFormat.jpg,
-      compressQuality: 100,
+      compressQuality: 80,
       uiSettings: [
         AndroidUiSettings(
             toolbarTitle: 'Cropper',
@@ -91,5 +59,11 @@ class SignUpProfileViewModel extends ChangeNotifier {
     return croppedFile?.path;
   }
 
-
+  Future<void> saveImage() async {
+    await authentication.storage.from('user_profile_images').upload(
+          'public/${authentication.auth.currentUser?.id}',
+          File(image!.path),
+          fileOptions: const FileOptions(upsert: true),
+        );
   }
+}
