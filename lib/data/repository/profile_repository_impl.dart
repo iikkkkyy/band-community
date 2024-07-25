bbdea1db9f72e193f61dcb7296d324abb55e5cc9
kimkangtae <tt997768@gmail.com>
2024-07-26 00:16:50 +0900

    Feat: Profile MVVM 패턴 적용

---
+++ lib/data/repository/profile_repository_impl.dart
@@ -0,0 +1,53 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:band_community/domain/repository/profile_repository.dart';
+import 'dart:io';
+
+import '../../domain/model/profile/region_model.dart';
+
+class ProfileRepositoryImpl implements ProfileRepository {
+  final SupabaseClient client;
+
+  ProfileRepositoryImpl({required this.client});
+
+  @override
+  Future<void> saveImage(File image, String userId) async {
+    await client.storage.from('user_profile_images').upload(
+      'public/$userId',
+      image,
+      fileOptions: const FileOptions(upsert: true),
+    );
+  }
+
+  @override
+  Future<List<Region>> getRegions() async {
+    final response = await client.from('regions').select();
+    final data = response;
+
+    final Map<String, List<String>> groupedData = {};
+
+    for (var item in data) {
+      final province = item['province'];
+      final city = item['city'];
+
+      if (groupedData.containsKey(province)) {
+        groupedData[province]?.add(city);
+      } else {
+        groupedData[province] = [city];
+      }
+    }
+
+    return groupedData.entries
+        .map((entry) => Region(province: entry.key, cities: entry.value))
+        .toList();
+  }
+
+  @override
+  Future<void> saveProfile(String userId, String introduction, String region, List<String> sessions) async {
+    await client.from('user_profile').upsert({
+      'user_id': userId,
+      'introduction': introduction,
+      'region': region,
+      'sessions': sessions,
+    });
+  }
+}
