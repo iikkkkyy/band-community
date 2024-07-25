6dca91f214ebbae427b1b051950ab87030199bf8
kimkangtae <tt997768@gmail.com>
2024-07-26 00:16:37 +0900

    Feat: 지역 정보 기능 추가

--- lib/domain/repository/profile_repository.dart
+++ lib/domain/repository/profile_repository.dart
@@ -1,6 +1,9 @@
 import 'dart:io';
 
+import '../model/profile/region_model.dart';
+
 abstract class ProfileRepository {
   Future<void> saveImage(File image, String userId);
-  Future<bool> checkUserNameExists(String userName);
+  Future<List<Region>> getRegions();
+  Future<void> saveProfile(String userId, String introduction, String region, List<String> sessions);
 }
