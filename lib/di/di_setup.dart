2d341d142548f17312b615c24f4048acaede4b05
kimkangtae <tt997768@gmail.com>
2024-07-25 17:09:16 +0900

    Feat : use case 추가

--- lib/di/di_setup.dart
+++ lib/di/di_setup.dart
@@ -1,5 +1,5 @@
-import 'package:band_community/domain/use_case/sign_in_use_case.dart';
-import 'package:band_community/domain/use_case/sign_in_with_kakao_use_case.dart';
+import 'package:band_community/domain/use_case/auth/sign_in_use_case.dart';
+import 'package:band_community/domain/use_case/auth/sign_in_with_kakao_use_case.dart';
 import 'package:band_community/presentation/mainpage/main_page_view_model.dart';
 import 'package:band_community/presentation/login/login_view_model.dart';
 import 'package:band_community/presentation/profile/profile_view_model.dart';
@@ -8,8 +8,10 @@ import 'package:band_community/presentation/signup/signup_screen_view_model.dart
 import 'package:get_it/get_it.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-import '../data/repository/user_login_repository_impl.dart';
-import '../domain/repository/user_login_repository.dart';
+import '../data/repository/auth_repository_impl.dart';
+import '../domain/repository/auth_repository.dart';
+import '../domain/use_case/auth/check_user_name_exists_use_case.dart';
+import '../domain/use_case/auth/sign_up_use_case.dart';
 
 final getIt = GetIt.instance;
 
@@ -18,14 +20,20 @@ void diSetup() {
   getIt.registerSingleton<SupabaseClient>(Supabase.instance.client);
 
   // Repositories
-  getIt.registerFactory<UserLoginRepository>(() => UserLoginRepositoryImpl(client: getIt<SupabaseClient>()));
+  getIt.registerFactory<AuthRepository>(() => AuthRepositoryImpl(client: getIt<SupabaseClient>()));
 
   // UseCases
-  getIt.registerFactory(() => SignInUseCase(getIt<UserLoginRepository>()));
-  getIt.registerFactory(() => SignInWithKakaoUseCase(getIt<UserLoginRepository>()));
+  getIt.registerFactory(() => SignUpUseCase(getIt<AuthRepository>()));
+  getIt.registerFactory(() => SignInUseCase(getIt<AuthRepository>()));
+  getIt.registerFactory(() => CheckUserNameExistsUseCase(getIt<AuthRepository>()));
+  getIt.registerFactory(() => SignInWithKakaoUseCase(getIt<AuthRepository>()));
 
   // ViewModels
-  getIt.registerFactory(() => SignUpViewModel());
+  getIt.registerFactory(() => SignUpViewModel(
+    signUpUseCase: getIt<SignUpUseCase>(),
+    signInUseCase: getIt<SignInUseCase>(),
+    checkUserNameExistsUseCase: getIt<CheckUserNameExistsUseCase>(),
+  ));
   getIt.registerFactory(() => LoginViewModel(
       signInUseCase: getIt<SignInUseCase>(),
       signInWithKakaoUseCase: getIt<SignInWithKakaoUseCase>()
