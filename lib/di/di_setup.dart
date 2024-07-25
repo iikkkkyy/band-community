ec26ba3d6d38915653760bc869b6d68fedee863b
kimkangtae <tt997768@gmail.com>
2024-07-26 00:15:42 +0900

    Feat: Profile MVVM 패턴 적용

--- lib/di/di_setup.dart
+++ lib/di/di_setup.dart
@@ -9,9 +9,14 @@ import 'package:get_it/get_it.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../data/repository/auth_repository_impl.dart';
+import '../data/repository/profile_repository_impl.dart';
 import '../domain/repository/auth_repository.dart';
+import '../domain/repository/profile_repository.dart';
 import '../domain/use_case/auth/check_user_name_exists_use_case.dart';
 import '../domain/use_case/auth/sign_up_use_case.dart';
+import '../domain/use_case/get_regions_use_case.dart';
+import '../domain/use_case/save_image_use_case.dart';
+import '../domain/use_case/save_profile_use_case.dart';
 
 final getIt = GetIt.instance;
 
@@ -21,12 +26,17 @@ void diSetup() {
 
   // Repositories
   getIt.registerFactory<AuthRepository>(() => AuthRepositoryImpl(client: getIt<SupabaseClient>()));
+  getIt.registerFactory<ProfileRepository>(() => ProfileRepositoryImpl(client: getIt<SupabaseClient>()));
+
 
   // UseCases
   getIt.registerFactory(() => SignUpUseCase(getIt<AuthRepository>()));
   getIt.registerFactory(() => SignInUseCase(getIt<AuthRepository>()));
   getIt.registerFactory(() => CheckUserNameExistsUseCase(getIt<AuthRepository>()));
   getIt.registerFactory(() => SignInWithKakaoUseCase(getIt<AuthRepository>()));
+  getIt.registerFactory(() => SaveImageUseCase(getIt<ProfileRepository>()));
+  getIt.registerFactory(() => GetRegionsUseCase(getIt<ProfileRepository>()));
+  getIt.registerFactory(() => SaveProfileUseCase(getIt<ProfileRepository>()));
 
   // ViewModels
   getIt.registerFactory(() => SignUpViewModel(
@@ -35,10 +45,14 @@ void diSetup() {
     checkUserNameExistsUseCase: getIt<CheckUserNameExistsUseCase>(),
   ));
   getIt.registerFactory(() => LoginViewModel(
-      signInUseCase: getIt<SignInUseCase>(),
-      signInWithKakaoUseCase: getIt<SignInWithKakaoUseCase>()
+    signInUseCase: getIt<SignInUseCase>(),
+    signInWithKakaoUseCase: getIt<SignInWithKakaoUseCase>(),
   ));
   getIt.registerFactory(() => ResetPasswordViewModel());
-  getIt.registerFactory(() => SignUpProfileViewModel());
+  getIt.registerFactory(() => SignUpProfileViewModel(
+    saveImageUseCase: getIt<SaveImageUseCase>(),
+    getRegionsUseCase: getIt<GetRegionsUseCase>(),
+    saveProfileUseCase: getIt<SaveProfileUseCase>(),
+  ));
   getIt.registerFactory(() => MainViewModel());
 }
