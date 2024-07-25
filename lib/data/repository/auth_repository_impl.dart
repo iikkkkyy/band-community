10b61664731fa90ee1ff8e8bc1550f898549cc4f
kimkangtae <tt997768@gmail.com>
2024-07-25 16:47:03 +0900

    Refactor : auth 관련 repository 통합

---
+++ lib/data/repository/auth_repository_impl.dart
@@ -0,0 +1,34 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import '../../domain/repository/auth_repository.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  final SupabaseClient client;
+
+
+  const AuthRepositoryImpl({
+    required this.client,
+  });
+
+  @override
+  Future<void> signInWithEmailAndPassword(String email, String password) async {
+    await client.auth.signInWithPassword(email: email, password: password);
+  }
+
+  @override
+  Future<void> signInWithKakao() async {
+    await client.auth.signInWithOAuth(OAuthProvider.kakao);
+  }
+
+  @override
+  Future<void> signUp(String email, String password, Map<String, dynamic> data) async {
+    await client.auth.signUp(email: email, password: password, data: data);
+  }
+
+  @override
+  Future<bool> checkUserNameExists(String userName) async {
+    final query = await client.from('user_profile').select('id').eq('user_name', userName).limit(1);
+    return query.isEmpty || (query == "[]");
+  }
+
+}
