d9f545773fc79917e14949bb47a53402efa5b6b4
kimkangtae <tt997768@gmail.com>
2024-07-31 19:53:42 +0900

    Feat: main page profile image 불러오기

--- lib/presentation/mainpage/main_page_view_model.dart
+++ lib/presentation/mainpage/main_page_view_model.dart
@@ -1,7 +1,20 @@
+import 'package:band_community/domain/use_case/get_user_profile_image_use_case.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class MainViewModel extends ChangeNotifier {
   final authentication = Supabase.instance.client;
+  final GetUserProfileImageUseCase _getUserProfileImageUseCase;
 
+  MainViewModel(this._getUserProfileImageUseCase);
+
+  String? userProfileImageUrl;
+
+  Future<void> loadUserProfileImage() async {
+    final userId = Supabase.instance.client.auth.currentUser?.id;
+    if (userId == null) return;
+
+    userProfileImageUrl = await _getUserProfileImageUseCase.execute(userId);
+    notifyListeners();
+  }
 }
