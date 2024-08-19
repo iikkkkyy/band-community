e017bf212d26e0002d73456e4bee7c10465fb5bb
kimkangtae <tt997768@gmail.com>
2024-08-20 01:55:10 +0900

    Feat: create bvand 추가

--- lib/di/di_setup.dart
+++ lib/di/di_setup.dart
@@ -5,19 +5,23 @@ import 'package:get_it/get_it.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../data/repository/auth_repository_impl.dart';
+import '../data/repository/band_repository_impl.dart';
 import '../data/repository/profile_repository_impl.dart';
 import '../domain/repository/auth_repository.dart';
+import '../domain/repository/band_repository.dart';
 import '../domain/repository/profile_repository.dart';
 import '../domain/use_case/auth/check_user_name_exists_use_case.dart';
 import '../domain/use_case/auth/sign_up_use_case.dart';
 import '../domain/use_case/get_regions_use_case.dart';
 import '../domain/use_case/get_user_profile_image_use_case.dart';
+import '../domain/use_case/main/create_band_use_case.dart';
 import '../domain/use_case/save_image_use_case.dart';
 import '../domain/use_case/save_profile_use_case.dart';
 import '../presentation/authentication/login/login_view_model.dart';
 import '../presentation/authentication/profile/profile_view_model.dart';
 import '../presentation/authentication/resetpassword/reset_password_view_model.dart';
 import '../presentation/authentication/signup/signup_screen_view_model.dart';
+import '../presentation/mainpage/create_band/create_band_view_model.dart';
 
 final getIt = GetIt.instance;
 
@@ -26,35 +30,46 @@ void diSetup() {
   getIt.registerSingleton<SupabaseClient>(Supabase.instance.client);
 
   // Repositories
-  getIt.registerFactory<AuthRepository>(() => AuthRepositoryImpl(client: getIt<SupabaseClient>()));
-  getIt.registerFactory<ProfileRepository>(() => ProfileRepositoryImpl(client: getIt<SupabaseClient>()));
-
+  getIt.registerFactory<AuthRepository>(
+      () => AuthRepositoryImpl(client: getIt<SupabaseClient>()));
+  getIt.registerFactory<ProfileRepository>(
+      () => ProfileRepositoryImpl(client: getIt<SupabaseClient>()));
+  getIt.registerFactory<BandRepository>(
+      () => BandRepositoryImpl(getIt<SupabaseClient>()));
 
   // UseCases
   getIt.registerFactory(() => SignUpUseCase(getIt<AuthRepository>()));
   getIt.registerFactory(() => SignInUseCase(getIt<AuthRepository>()));
-  getIt.registerFactory(() => CheckUserNameExistsUseCase(getIt<AuthRepository>()));
+  getIt.registerFactory(
+      () => CheckUserNameExistsUseCase(getIt<AuthRepository>()));
   getIt.registerFactory(() => SignInWithKakaoUseCase(getIt<AuthRepository>()));
   getIt.registerFactory(() => SaveImageUseCase(getIt<ProfileRepository>()));
   getIt.registerFactory(() => GetRegionsUseCase(getIt<ProfileRepository>()));
   getIt.registerFactory(() => SaveProfileUseCase(getIt<ProfileRepository>()));
-  getIt.registerFactory(() => GetUserProfileImageUseCase(getIt<ProfileRepository>()));
+  getIt.registerFactory(
+      () => GetUserProfileImageUseCase(getIt<ProfileRepository>()));
+  getIt.registerFactory(() => CreateBandUseCase(getIt<BandRepository>()));
 
   // ViewModels
   getIt.registerFactory(() => SignUpViewModel(
-    signUpUseCase: getIt<SignUpUseCase>(),
-    signInUseCase: getIt<SignInUseCase>(),
-    checkUserNameExistsUseCase: getIt<CheckUserNameExistsUseCase>(),
-  ));
+        signUpUseCase: getIt<SignUpUseCase>(),
+        signInUseCase: getIt<SignInUseCase>(),
+        checkUserNameExistsUseCase: getIt<CheckUserNameExistsUseCase>(),
+      ));
   getIt.registerFactory(() => LoginViewModel(
-    signInUseCase: getIt<SignInUseCase>(),
-    signInWithKakaoUseCase: getIt<SignInWithKakaoUseCase>(),
-  ));
+        signInUseCase: getIt<SignInUseCase>(),
+        signInWithKakaoUseCase: getIt<SignInWithKakaoUseCase>(),
+      ));
   getIt.registerFactory(() => ResetPasswordViewModel());
   getIt.registerFactory(() => SignUpProfileViewModel(
-    saveImageUseCase: getIt<SaveImageUseCase>(),
-    getRegionsUseCase: getIt<GetRegionsUseCase>(),
-    saveProfileUseCase: getIt<SaveProfileUseCase>(),
-  ));
-  getIt.registerFactory(() => MainViewModel(getIt<GetUserProfileImageUseCase>()));
+        saveImageUseCase: getIt<SaveImageUseCase>(),
+        getRegionsUseCase: getIt<GetRegionsUseCase>(),
+        saveProfileUseCase: getIt<SaveProfileUseCase>(),
+      ));
+  getIt.registerFactory(
+      () => MainViewModel(getIt<GetUserProfileImageUseCase>()));
+
+  // CreateBandViewModel에 두 개의 인자를 전달합니다.
+  getIt.registerFactory(() => CreateBandViewModel(
+      getIt<CreateBandUseCase>(), getIt<SaveImageUseCase>()));
 }
