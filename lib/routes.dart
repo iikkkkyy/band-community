b3f55cb686958f68e78f34323038caf1fa932790
kimkangtae <tt997768@gmail.com>
2024-08-20 00:21:53 +0900

    Feat: presentation 폴더 구조 변 및 create band 페이지 추가

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,13 +1,13 @@
-import 'package:band_community/presentation/login/login_screen.dart';
-import 'package:band_community/presentation/login/login_view_model.dart';
+import 'package:band_community/presentation/authentication/login/login_screen.dart';
+import 'package:band_community/presentation/authentication/login/login_view_model.dart';
+import 'package:band_community/presentation/authentication/profile/profile_screen.dart';
+import 'package:band_community/presentation/authentication/profile/profile_view_model.dart';
+import 'package:band_community/presentation/authentication/resetpassword/reset_password_screen.dart';
+import 'package:band_community/presentation/authentication/resetpassword/reset_password_view_model.dart';
+import 'package:band_community/presentation/authentication/signup/signup_screen.dart';
+import 'package:band_community/presentation/authentication/signup/signup_screen_view_model.dart';
 import 'package:band_community/presentation/mainpage/main_page_screen.dart';
 import 'package:band_community/presentation/mainpage/main_page_view_model.dart';
-import 'package:band_community/presentation/profile/profile_screen.dart';
-import 'package:band_community/presentation/profile/profile_view_model.dart';
-import 'package:band_community/presentation/resetpassword/reset_password_screen.dart';
-import 'package:band_community/presentation/resetpassword/reset_password_view_model.dart';
-import 'package:band_community/presentation/signup/signup_screen.dart';
-import 'package:band_community/presentation/signup/signup_screen_view_model.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'di/di_setup.dart';
@@ -49,6 +49,15 @@ final router = GoRouter(
         create: (_) => getIt<MainViewModel>(),
         child: const MainPageScreen(),
       ),
+      routes: [
+        GoRoute(
+          path: 'create-band',
+          builder: (context, state) => ChangeNotifierProvider(
+            create: (_) => getIt<CreateBandViewModel>(),
+            child: const CreateBandScreen(),
+          ),
+        ),
+      ],
     ),
   ],
 );
