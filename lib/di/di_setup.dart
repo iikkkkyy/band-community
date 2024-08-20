ac1beb688d277bd923c9c41cec5370e6d9017239
kimkangtae <tt997768@gmail.com>
2024-08-20 21:45:14 +0900

    Feat: Main Band관련 의존성 주입

--- lib/di/di_setup.dart
+++ lib/di/di_setup.dart
@@ -1,14 +1,18 @@
 import 'package:band_community/domain/use_case/auth/sign_in_use_case.dart';
 import 'package:band_community/domain/use_case/auth/sign_in_with_kakao_use_case.dart';
+import 'package:band_community/domain/use_case/get_group_profile_image_use_case.dart';
+import 'package:band_community/domain/use_case/main/get_band_use_case.dart';
 import 'package:band_community/presentation/mainpage/main_page_view_model.dart';
 import 'package:get_it/get_it.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../data/repository/auth_repository_impl.dart';
+import '../data/repository/band_create_repository_impl.dart';
 import '../data/repository/band_repository_impl.dart';
 import '../data/repository/profile_repository_impl.dart';
 import '../domain/repository/auth_repository.dart';
-import '../domain/repository/band_repository.dart';
+import '../domain/repository/band_create_repository.dart';
+import '../domain/repository/band_repository.dart'; // BandRepository 추가
 import '../domain/repository/profile_repository.dart';
 import '../domain/use_case/auth/check_user_name_exists_use_case.dart';
 import '../domain/use_case/auth/sign_up_use_case.dart';
@@ -34,21 +38,23 @@ void diSetup() {
       () => AuthRepositoryImpl(client: getIt<SupabaseClient>()));
   getIt.registerFactory<ProfileRepository>(
       () => ProfileRepositoryImpl(client: getIt<SupabaseClient>()));
+  getIt.registerFactory<BandCreateRepository>(
+      () => BandCreateRepositoryImpl(getIt<SupabaseClient>()));
   getIt.registerFactory<BandRepository>(
-      () => BandRepositoryImpl(getIt<SupabaseClient>()));
+      () => BandRepositoryImpl(getIt<SupabaseClient>())); // BandRepository 추가
 
   // UseCases
   getIt.registerFactory(() => SignUpUseCase(getIt<AuthRepository>()));
   getIt.registerFactory(() => SignInUseCase(getIt<AuthRepository>()));
-  getIt.registerFactory(
-      () => CheckUserNameExistsUseCase(getIt<AuthRepository>()));
+  getIt.registerFactory(() => CheckUserNameExistsUseCase(getIt<AuthRepository>()));
   getIt.registerFactory(() => SignInWithKakaoUseCase(getIt<AuthRepository>()));
   getIt.registerFactory(() => SaveImageUseCase(getIt<ProfileRepository>()));
   getIt.registerFactory(() => GetRegionsUseCase(getIt<ProfileRepository>()));
   getIt.registerFactory(() => SaveProfileUseCase(getIt<ProfileRepository>()));
-  getIt.registerFactory(
-      () => GetUserProfileImageUseCase(getIt<ProfileRepository>()));
-  getIt.registerFactory(() => CreateBandUseCase(getIt<BandRepository>()));
+  getIt.registerFactory(() => GetUserProfileImageUseCase(getIt<ProfileRepository>()));
+  getIt.registerFactory(() => CreateBandUseCase(getIt<BandCreateRepository>()));
+  getIt.registerFactory(() => GetBandsUseCase(getIt<BandRepository>()));
+  getIt.registerFactory(() => GetGroupProfileImageUseCase(getIt<ProfileRepository>())); // GetBandsUseCase 추가
 
   // ViewModels
   getIt.registerFactory(() => SignUpViewModel(
@@ -66,8 +72,8 @@ void diSetup() {
         getRegionsUseCase: getIt<GetRegionsUseCase>(),
         saveProfileUseCase: getIt<SaveProfileUseCase>(),
       ));
-  getIt.registerFactory(
-      () => MainViewModel(getIt<GetUserProfileImageUseCase>()));
+  getIt.registerFactory(() => MainViewModel(
+      getIt<GetUserProfileImageUseCase>(),getIt<GetGroupProfileImageUseCase>(), getIt<GetBandsUseCase>()));
 
   // CreateBandViewModel에 두 개의 인자를 전달합니다.
   getIt.registerFactory(() => CreateBandViewModel(
