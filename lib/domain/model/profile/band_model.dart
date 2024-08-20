041e14429cfabf97121bc4a76d4d81ff2f1f3a87
kimkangtae <tt997768@gmail.com>
2024-08-20 21:43:57 +0900

    Feat: band model 생성

---
+++ lib/domain/model/profile/band_model.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/foundation.dart';
+
+class Band {
+  final String id;
+  final String name;
+  final String introduce;
+  // final Uint8List image;
+
+  Band({
+    required this.id,
+    required this.name,
+    required this.introduce,
+    // required this.image,
+  });
+}
